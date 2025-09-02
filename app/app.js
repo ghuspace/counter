0ed9847346ae7e5d7226883da1c9a5538c49cb4e
Gustavo De Longhi <gustavodelonghi@gmail.com>
2025-09-02 10:53:57 -0300

    feat: more ways to count (fibonacci, romans, prime numbers)

--- app/app.js
+++ app/app.js
@@ -1,10 +1,15 @@
 // Variáveis
-
 let count = 0;
 let mode = "comum";
 const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
 const options = document.querySelectorAll(".navbarlateral a");
+
+//fibonacci config
+let fibonacci_sequence = [1, 1, 2]
+let fibonacci_index = 0 //vai de 0 a 2
+let decreased = false;
+let increased = false;
 
 // Lógica das opções
 options.forEach(item => {
@@ -13,6 +18,14 @@ options.forEach(item => {
     mode = e.currentTarget.id;
     if (mode == "impar"){
       count = 1;
+    } else if (mode == "primos"){
+      count = 2;
+    } else if (mode == "romano") {
+      count = "I"
+    } else if (mode == "fibonacci") {
+      decreased = false
+      increased = false
+      count = 0
     } else {
       count = 0;
     }
@@ -23,6 +36,7 @@ options.forEach(item => {
 // Ainda preciso colocar pra destacar a corzinha da opção la no menu, qnd o usuário escolhe
 btns.forEach(function (btn) {
   btn.addEventListener("click", function (e) {
+    
     const styles = e.currentTarget.classList;
 
     let option;
@@ -49,6 +63,18 @@ btns.forEach(function (btn) {
 
       case "aleatorio":
         count = randomCount(count, option);
+        break;
+      
+      case "fibonacci":
+        count = fibonacciCount(count, option);
+        break;
+
+      case "primos":
+        count = primeCount(count, option);
+        break;
+      
+      case "romano":
+        count = romanCount(count, option);
         break;
     }
     value.textContent = count;
@@ -95,4 +121,174 @@ function randomCount(count, option) {
   return 0;
 }
 
-// To tentando deixar padronizado em ingles o codigo, se achar alguma caca ai pode me falarKKKKKKKKKK
+//fibonacci
+function fibonacciCount(count, option) {
+  if (option === "decrease") {
+    decreased = true;
+    if(increased) {
+      increased = false
+      fibonacci_index--;
+    }
+    if(count < 2) {
+      if (count == 0) {
+        count = 0
+        fibonacci_index=0
+        fibonacci_sequence = [1, 1, 2]
+      } else {
+        count--;
+        fibonacci_index--;
+      }
+    } else {
+      if (fibonacci_index == 0){
+        fibonacci_sequence[0] = fibonacci_sequence[2] - Math.abs(fibonacci_sequence[1])
+        count = fibonacci_sequence[2];
+        fibonacci_index = 2
+      } else if (fibonacci_index == 1){
+        fibonacci_sequence[1] = fibonacci_sequence[0] - Math.abs(fibonacci_sequence[2])
+        count = fibonacci_sequence[0];
+        fibonacci_index = 0
+      }
+      else{ 
+        fibonacci_sequence[2] = fibonacci_sequence[1] - Math.abs(fibonacci_sequence[0])
+        count = fibonacci_sequence[1];
+        fibonacci_index = 1
+      }
+    }
+  } else if (option == "increase") {
+    increased = true
+    if(decreased) {
+      decreased = false
+      fibonacci_index++;
+    }
+    if(count < 2) {
+      if (fibonacci_index != 2) {
+        count = fibonacci_sequence[fibonacci_index]
+        fibonacci_index++;
+      } else {
+        fibonacci_index = 0
+        count = fibonacci_sequence[2]
+      }
+    } else {
+      if (fibonacci_index == 0){
+        fibonacci_sequence[0] = fibonacci_sequence[1] + fibonacci_sequence[2]
+        count = fibonacci_sequence[0];
+        fibonacci_index = 1
+      } else if (fibonacci_index == 1){
+        fibonacci_sequence[1] = fibonacci_sequence[2] + fibonacci_sequence[0]
+        count = fibonacci_sequence[1];
+        fibonacci_index = 2
+      }
+      else{ 
+        fibonacci_sequence[2] = fibonacci_sequence[1] + fibonacci_sequence[0]
+        count = fibonacci_sequence[2];
+        fibonacci_index = 0
+      }
+    }
+  } else {
+    count = 0;
+    fibonacci_index = 0 
+    fibonacci_sequence = [1, 1, 2]
+  } 
+  return count
+}
+
+function primeCount(count, option) {
+  if(option == "decrease"){
+    let prime = true
+    while (prime){
+      count--;
+      if(isItPrime(count)){
+        prime = false
+      }
+    }
+  }
+  else if(option == "increase") {
+    let prime = true
+    while (prime){
+      count++;
+      if(isItPrime(count)){
+        prime = false
+      }
+    }
+  }
+  else {
+    count = 2
+  }
+
+  return count
+}
+
+function isItPrime(number){
+  if(number == 1 || number == 0){
+    return false
+  }
+
+  if(number > 0 ){
+    for(let i = 0; i<number; i++){
+      if(number%i==0) {
+        if(number%i==0 && i!=1 && i!=number){
+          return false;
+        }
+      }
+    }
+  } else {
+    let numberPositive = number*(-1)
+    for(let i = 0; i<numberPositive; i++){
+      if(numberPositive%i==0) {
+        if(numberPositive%i==0 && i!=1 && i!=numberPositive){
+          return false;
+        }
+      }
+    }
+  }
+
+
+  return true;
+}
+
+function romanCount(count, option){
+  if(option == "decrease"){
+    if(count == "I"){
+      return "I";
+    }
+    let number = fromRoman(count)
+    number--;
+    count = toRoman(number)
+  }else if(option == "increase"){
+    let number = fromRoman(count)
+    number++;
+    count = toRoman(number)
+  }
+  else{
+    count = "I"
+  }
+  
+  return count;
+}
+
+function toRoman(num) {
+  const algarisms = [
+    [1000, 'M'], [900, 'CM'], [500, 'D'], [400, 'CD'],
+    [100, 'C'], [90, 'XC'], [50, 'L'], [40, 'XL'],
+    [10, 'X'], [9, 'IX'], [5, 'V'], [4, 'IV'], [1, 'I']
+  ];
+  let result = '';
+  for (const [value, numeral] of algarisms) {
+    while (num >= value) {
+      result += numeral;
+      num -= value;
+    }
+  }
+  return result;
+}
+
+function fromRoman(str) {
+  const algarisms = {I:1, V:5, X:10, L:50, C:100, D:500, M:1000};
+  let num = 0;
+  for (let i = 0; i < str.length; i++) {
+    const curr = algarisms[str[i]], next = algarisms[str[i+1]];
+    if (next > curr) num -= curr;
+    else num += curr;
+  }
+  return num;
+}
